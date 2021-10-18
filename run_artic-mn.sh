7fd7f6a15b47b4f6121842eda7299f57a7992921
Jake Garfin <garfinjm@gmail.com>
2021-10-18 14:39:26 -0500

    adjust containers and resources

--- run_artic-mn.sh
+++ run_artic-mn.sh
@@ -1,13 +1,33 @@
-#!/bin/bash -l
+#!/usr/bin/env bash
 
 set -e
+
+primerset=$1
+extra_config=""
+
+# Make sure a valid primer set is given
+if [ "${primerset}" = "V1" ] || [ "${primerset}" = "V2" ] || [ "${primerset}" = "V3" ] || [ "${primerset}" = "V4" ] || [ "${primerset}" = "V1200" ]
+then
+  echo "Running analysis with ${primerset} primers"
+else
+  echo -e "\nInvalid Primerset:${primerset}\n\nPick one:\nV1\nV2\nV3\nV4\nV1200"
+  exit 1
+fi
+
+# Additional processing for V1200
+if [ "${primerset}" = "V1200" ]
+then
+  extra_config="-c /home/mdh/shared/software_modules/artic-mn/artic-mn/midnight.config"
+fi
 
 export NXF_OFFLINE='TRUE'
 
 run_name=$(basename $(pwd))
 
 nextflow /home/mdh/shared/software_modules/artic-mn/artic-mn/artic-mn.nf \
+  ${extra_config} \
   -c /home/mdh/shared/software_modules/artic-mn/artic-mn/artic-mn.config \
+  --primers ${primerset} \
   --fastq_dir fastq_pass/ \
   -with-report ${run_name}/nf_report.html \
   -with-trace ${run_name}/nf_trace.tsv \
