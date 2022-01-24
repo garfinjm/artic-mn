5f64a8806617ae47b2c4164f5a1c9d9cc36cbd92
Jake Garfin <garfinjm@gmail.com>
2022-01-24 14:35:49 -0600

    add check for sequencing artifact near pos 19200

--- run_artic-mn.sh
+++ run_artic-mn.sh
@@ -40,11 +40,30 @@ nextflow /home/mdh/shared/software_modules/artic-mn/artic-mn/artic-mn.nf \
 # Look for failed samples
 awk -F '\t' '$5 == "FAILED" && $4 !~ /\(NTC/ {print $5,$4,$6,$7,$2}' ${run_name}/nf_trace.tsv | sort > ${run_name}/errors_${run_name}.tsv
 repeats=$(cut -d' ' -f 3 ${run_name}/errors_${run_name}.tsv | uniq | tr -d '(),')
-if [ "$repeats" ];
-then
+if [ "$repeats" ]; then
   echo -e "\n\033[0;31mCareful, it looks like the samples below had errors during analysis:";
   echo -e "$repeats";
   echo -e "\n\033[0m";
+fi
+
+
+# Flag samples with known sequencing issues
+if [ -d "./${run_name}/vcfs/pass" ] || [ -d "./${run_name}/vcfs/review" ]; then
+  echo "Careful, the read pileups for the samples below need review near position 19210:"
+fi
+
+if [ -d "./${run_name}/vcfs/pass" ]; then
+  for file in ./${run_name}/vcfs/pass/*pass.vcf
+  do
+    awk '(($2 == 19209 || $2 == 19212 || $2 == 19214)&&($5 == "G" || $5 == "GG"))||($2 == 19217 && $5 == "A") {print FILENAME; exit}' $file | cut -d"/" -f5 | cut -d"_" -f1
+  done
+fi
+
+if [ -d "./${run_name}/vcfs/review" ]; then
+  for file in ./${run_name}/vcfs/review/*pass.vcf
+  do
+    awk '(($2 == 19209 || $2 == 19212 || $2 == 19214)&&($5 == "G" || $5 == "GG"))||($2 == 19217 && $5 == "A") {print FILENAME; exit}' $file | cut -d"/" -f5 | cut -d"_" -f1
+  done
 fi
 
 # Remove pangolin
